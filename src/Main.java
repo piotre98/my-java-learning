b3ef6b5176441d6af4d883625b6760e548965120
pbole98 <p.boleszczuk@wp.pl>
2024-10-01 22:21:46 +0200

    Another commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Pies");
+        //System.out.print("Pies");
+        //System.out.print("Okrutnik");
+
+        int x; //declaration
+
+        x = 123; //assignment
+
+        int y = 123; //initialization
+
+        float z = 3.14f; //"f" at the end for float
+
+        double c = 3.14; //double doesnt need f or d at the end
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Papieżu, jak masz na imię? ");
+        String name = scanner.nextLine();
+        System.out.println("A czy papież lubi zęby myć? ");
+        boolean trueorfalse = scanner.nextBoolean();
+
+        System.out.println("Witaj " +name);
+        System.out.println("Czy papież lubi zęby myć? Odpowiedź: " +trueorfalse);
 
     }
 }
