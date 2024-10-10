143cbc74a7d3148a773cb58b1d1854057e710591
pbole98 <p.boleszczuk@wp.pl>
2024-10-10 21:49:44 +0200

    10.10.2024 commit

---
+++ src/switches.java
@@ -0,0 +1,19 @@
+public class switches {
+
+    public static void main(String[] args) {
+
+        //switch - a statement that allows a variable to be tested for equality against a list of values
+
+        String cat = "British";
+
+        switch(cat) {
+            case "Russian": System.out.println("It is Russian");
+            break;
+            case "Sphinx": System.out.println("It is Sphinx");
+            break;
+            case "British": System.out.println("It is British");
+            break;
+        }
+
+    }
+}
