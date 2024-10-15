988a37454137669f73d3ba03291da50f2a7446ed
pbole98 <p.boleszczuk@wp.pl>
2024-10-15 20:07:19 +0200

    Another commit

---
+++ src/while_loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class while_loop {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while(name.isBlank()) {
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello "+name);
+
+    }
+}
