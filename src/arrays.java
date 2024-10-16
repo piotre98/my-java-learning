a5244ef18c556b636074c62e6061c401a6478436
pbole98 <p.boleszczuk@wp.pl>
2024-10-16 22:59:30 +0200

    Another

---
+++ src/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+
+    public static void main(String[] args) {
+
+        //array is used to store multiple values in a single variable
+
+        String[] cars = {"Camaro, Corvette. Citroen"};
+
+        cars[0] = "Mustang";
+
+        System.out.println(cars[0]);
+
+    }
+
+}
