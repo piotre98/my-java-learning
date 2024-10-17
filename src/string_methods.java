a339e5901d9917d958d6b1874c002872ab11c547
pbole98 <p.boleszczuk@wp.pl>
2024-10-17 23:38:47 +0200

    Another commit

---
+++ src/string_methods.java
@@ -0,0 +1,13 @@
+public class string_methods {
+
+    public static void main(String[] args){
+
+        String name="Papaj";
+
+        boolean result = name.equals("Papaj");
+
+        System.out.println(result);
+
+    }
+
+}
