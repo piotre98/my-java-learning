b93ef61ecfd3b3d6c752a2afde4aaf92e425ed1d
pbole98 <p.boleszczuk@wp.pl>
2024-10-22 23:21:26 +0200

    Another commit

---
+++ src/wrapper_classes.java
@@ -0,0 +1,17 @@
+public class wrapper_classes {
+
+    public static void main(String[] args){
+
+        Boolean a = false;
+        Character b = '@';
+        Integer c = 1;
+        Double d = 3.14;
+        String e = "Doge";
+
+        if(a==true) {
+            System.out.println("This is true");
+        }
+
+    }
+
+}
