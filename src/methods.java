eec045c39ca3bc6ce2130476a2da24f90c844c84
pbole98 <p.boleszczuk@wp.pl>
2024-11-14 11:11:51 +0100

    Another commit

---
+++ src/methods.java
@@ -0,0 +1,17 @@
+public class methods {
+
+    public static void main(String[] args){
+
+        String name = "Papjeż";
+
+        hello(name);
+
+    }
+
+     static void hello(String name){
+
+        System.out.println("Hello "+name);
+
+    }
+
+}
